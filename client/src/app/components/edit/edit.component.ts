7e09a98c28406ac51610a24e22f1af2f866b4d32
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-20 23:34:39 -0700

    added components

---
+++ client/src/app/components/edit/edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-component',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent { }
