dad3971b7ccec2f2ed6505735721e1937850c8cf
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-23 15:12:23 -0700

    added additional routes

--- server.js
+++ server.js
@@ -40,8 +40,49 @@ router.route('/issues/:id').get((req, res) => {
 
 router.route('/issues/add').post((req, res) => {
   const issue = new Issue(req.body);
-  issue.save().then(issue => {
-    res.status(200).json({ success: 'issue added successfully' });
+  issue
+    .save()
+    .then(issue => {
+      console.log(issue);
+      res.status(200).json({ success: 'issue added successfully' });
+    })
+    .catch(err => {
+      res.status(400).send('Issue adding your issue', err);
+    });
+});
+
+router.route('/issues/update/:id').post((req, res) => {
+  Issue.findById(req.params.id, (err, issue) => {
+    if (err) {
+      res.json(err);
+    } else if (!issue) {
+      res.status(500).send('issue not found');
+    } else {
+      res.status(200);
+      issue.title = req.body.title;
+      issue.respondible = req.body.responsible;
+      issue.description = req.body.description;
+      issue.severity = req.body.severity;
+      issue.status = req.body.status;
+
+      issue
+        .save()
+        .then(() => res.json('Updated'))
+        .catch(() => {
+          res.staus(400).send('unable to process request');
+        });
+    }
+  });
+});
+
+router.route('/issues/delete/:id').delete((req, res) => {
+  Issue.findByIdAndRemove({ _id: req.params.id }, (err, issue) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.status(200);
+      res.json(issue, 'successful');
+    }
   });
 });
 
