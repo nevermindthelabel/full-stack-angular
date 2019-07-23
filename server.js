5c92f5564f1fe0edd8339a5830d35a1b86957984
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-22 21:18:18 -0700

    added mongoose and models

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import Issue from './models/Issue';
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Welcome'));
+const router = express.Router();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost/MEANstack', { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => console.log('DB connected'));
+
+router.route('/issues').get((req, res) => {
+  Issue.find((err, issues) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.json(issues);
+    }
+  });
+});
+
+app.use('/', router);
+
 app.listen(PORT, () => console.log(`🏃‍  on http://localhost:${PORT}`));
