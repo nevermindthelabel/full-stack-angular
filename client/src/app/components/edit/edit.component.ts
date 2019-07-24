cc5660b540d2b196d5c0c86822cdd13499a72d7d
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-24 16:18:24 -0700

    added http service to edit component

--- client/src/app/components/edit/edit.component.ts
+++ client/src/app/components/edit/edit.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IssueService } from '../../issue.service';
 
 @Component({
   selector: 'app-edit-component',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent { }
+export class EditComponent implements OnInit {
+
+  constructor(private issueService: IssueService) { }
+
+  ngOnInit() {}
+}
