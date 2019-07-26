71e99fa8f3acf2f9f0375a983cdb166f4770fc35
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-26 10:22:54 -0700

    added form group and methods

--- client/src/app/components/create/create.component.ts
+++ client/src/app/components/create/create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IssueService } from '../../issue.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-create-component',
@@ -9,7 +12,25 @@ import { IssueService } from '../../issue.service';
 
 export class CreateComponent implements OnInit {
 
-  constructor(private issueService: IssueService) { }
+  createForm: FormGroup;
+
+  constructor(
+    private issueService: IssueService,
+    private fb: FormBuilder,
+    private router: Router) {
+    this.createForm = this.fb.group({
+      title: ['', Validators.required],
+      responsible: '',
+      description: '',
+      severity: ''
+    });
+  }
+
+  addIssue(title: string, responsible: string, description: string, severity: string) {
+    this.issueService.addIssue(title, responsible, description, severity).subscribe(() => {
+      this.router.navigate(['/list']);
+    });
+  }
 
   ngOnInit() { }
 
